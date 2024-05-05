e9493e5079029ee7e28a21e0964f08b913691395
Douglas Andrade <suportdoug@gmail.com>
2024-05-05 01:54:21 -0300

    wip: responsive crazyyyy :(

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LegendComponent } from './components/legend/legend.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FormsComponent,
+    FooterComponent,
+    LegendComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/legend/legend.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legend',
+  standalone: true,
+  imports: [],
+  templateUrl: './legend.component.html',
+  styleUrl: './legend.component.scss'
+})
+export class LegendComponent {
+
+}
