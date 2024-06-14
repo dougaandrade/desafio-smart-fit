4c5d0dd2b8ade51b188a4aca19c5f83e67eb7ec3
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-06-14 11:18:42 -0300

    wip:coisas novas vino

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -34,13 +34,71 @@ export class FormsComponent {
       showClosed ? true : undefined,
       typeof hour === 'string' ? hour : undefined
     );
+
     if (!uf) {
       return this.academias.emit(academias);
+    } else {
+      this.academias.emit(
+        academias.filter((value) => {
+          return value.uf === uf;
+        })
+      );
     }
+  }
+
+  async onMorging() {
+    const { hour } = this.formGroup.value;
+
+    const academias = await this.unitService.obterAcademias(
+      '',
+      typeof hour === 'string' ? hour : undefined
+    );
     this.academias.emit(
-      academias.filter((value) => {
-        return value.uf === uf;
+      academias.filter((academia) => {
+        return (
+          academia.schedules &&
+          academia.schedules.some((schedule) => schedule.hour !== '06h às 12h')
+        );
       })
+    );
+  }
+  async onAfternoon() {
+    const { hour } = this.formGroup.value;
+
+    const academias = await this.unitService.obterAcademias(
+      '',
+      typeof hour === 'string' ? hour : undefined
+    );
+    this.academias.emit(
+      academias.filter((academia) => {
+        return (
+          academia.schedules &&
+          academia.schedules.some((schedule) => schedule.hour !== '12h às 18h')
+        );
+      })
+    );
+  }
+  async onNight() {
+    const { hour } = this.formGroup.value;
+
+    const academias = await this.unitService.obterAcademias(
+      '',
+      typeof hour === 'string' ? hour : undefined
+    );
+    this.academias.emit(
+      academias.filter((academia) => {
+        return (
+          academia.schedules &&
+          academia.schedules.some((schedule) => schedule.hour == '18h')
+        );
+      })
+    );
+  }
+
+  async onMask() {
+    const academias = await this.unitService.obterAcademias();
+    this.academias.emit(
+      academias.filter((value) => value.mask === 'recommended')
     );
   }
 
@@ -48,3 +106,5 @@ export class FormsComponent {
     this.formGroup.reset();
   }
 }
+/// posso criar metodos para caso tenha mascara e afins mas tem que arrumar esse dos horarios
+// posso criar uma pasta com todos os metodos de filtro e no forms passar so um public com os metodos usados para assim diminuir os codigos e ajudar na manutenção do código incluido
