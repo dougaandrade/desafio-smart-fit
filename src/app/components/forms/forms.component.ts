c8cca8ec59bbdfd44414874b55beaa88c7fe8d00
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-06-19 11:41:13 -0300

    fature:results-filter

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,6 @@
 import { Academias } from './../../Interfaces/Ilocation.interface';
 import { CommonModule } from '@angular/common';
-import { Component, output } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { UF } from '../enum/locaisUf.enum';
@@ -10,33 +10,34 @@ import { UF } from '../enum/locaisUf.enum';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss',
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
-  readonly academias = output<Academias[]>();
+  @Output() academias = new EventEmitter<Academias[]>();
   formGroup = this.formBuilder.group({
     hour: '',
     showClosed: false,
     uf: '',
   });
   localUF = Object.values(UF);
+  resultadosCount = 0;
 
   constructor(
     private formBuilder: FormBuilder,
     private unitService: GetUnitsService
-  ) {}
+  ) {
+    this.loadAllAcademias();
+  }
 
-  // async onFullAcademias() {
-  //   const { hour, showClosed } = this.formGroup.value;
+  private updateResultadosCount(academias: Academias[]) {
+    this.resultadosCount = academias.length;
+  }
+  private async loadAllAcademias() {
+    const academias = await this.unitService.obterAcademias();
+    this.updateResultadosCount(academias);
+    this.academias.emit(academias);
+  }
 
-  //   const academias = await this.unitService.obterAcademias(
-  //     '',
-  //     '',
-  //     showClosed ? true : undefined,
-  //     typeof hour === 'string' ? hour : undefined
-  //   );
-  //   this.academias.emit(academias);
-  // }
   async onSearchLocal() {
     const { hour, showClosed, uf } = this.formGroup.value;
 
@@ -48,31 +49,33 @@ export class FormsComponent {
     );
 
     if (!uf) {
-      return this.academias.emit(academias);
+      this.updateResultadosCount(academias);
+      this.academias.emit(academias);
     } else {
-      this.academias.emit(
-        academias.filter((value) => {
-          return value.uf === uf;
-        })
-      );
+      const filteredAcademias = academias.filter((value) => value.uf === uf);
+      this.updateResultadosCount(filteredAcademias);
+      this.academias.emit(filteredAcademias);
     }
   }
-  async onMorging() {
+
+  async onMorning() {
     const { hour } = this.formGroup.value;
 
     const academias = await this.unitService.obterAcademias(
       '',
       typeof hour === 'string' ? hour : undefined
     );
-    this.academias.emit(
-      academias.filter((academia) => {
-        return (
-          academia.schedules &&
-          academia.schedules.some((schedule) => schedule.hour === '06h às 12h')
-        );
-      })
+
+    const filteredAcademias = academias.filter(
+      (academia) =>
+        academia.schedules &&
+        academia.schedules.some((schedule) => schedule.hour === '06h às 12h')
     );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onAfternoon() {
     const { hour } = this.formGroup.value;
 
@@ -80,15 +83,17 @@ export class FormsComponent {
       '',
       typeof hour === 'string' ? hour : undefined
     );
-    this.academias.emit(
-      academias.filter((academia) => {
-        return (
-          academia.schedules &&
-          academia.schedules.some((schedule) => schedule.hour === '12h às 18h')
-        );
-      })
+
+    const filteredAcademias = academias.filter(
+      (academia) =>
+        academia.schedules &&
+        academia.schedules.some((schedule) => schedule.hour === '12h às 18h')
     );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onNight() {
     const { hour } = this.formGroup.value;
 
@@ -96,37 +101,61 @@ export class FormsComponent {
       '',
       typeof hour === 'string' ? hour : undefined
     );
-    this.academias.emit(
-      academias.filter((academia) => {
-        return (
-          academia.schedules &&
-          academia.schedules.some((schedule) => schedule.hour === '18h às 21h')
-        );
-      })
+
+    const filteredAcademias = academias.filter(
+      (academia) =>
+        academia.schedules &&
+        academia.schedules.some((schedule) => schedule.hour === '18h às 21h')
     );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onFilterMask() {
     const academias = await this.unitService.obterAcademias();
-    this.academias.emit(academias.filter((value) => value.mask === 'required'));
+
+    const filteredAcademias = academias.filter(
+      (value) => value.mask === 'required'
+    );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onFilterTowel() {
     const academias = await this.unitService.obterAcademias();
-    this.academias.emit(
-      academias.filter((value) => value.towel === 'required')
+
+    const filteredAcademias = academias.filter(
+      (value) => value.towel === 'required'
     );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onFilterFountain() {
     const academias = await this.unitService.obterAcademias();
-    this.academias.emit(
-      academias.filter((value) => value.fountain === 'partial')
+
+    const filteredAcademias = academias.filter(
+      (value) => value.fountain === 'partial'
     );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onFilterLocker() {
     const academias = await this.unitService.obterAcademias();
-    this.academias.emit(
-      academias.filter((value) => value.locker_room === 'allowed')
+
+    const filteredAcademias = academias.filter(
+      (value) => value.locker_room === 'allowed'
     );
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
+
   async onShowClose() {
     const { showClosed } = this.formGroup.value;
 
@@ -136,12 +165,9 @@ export class FormsComponent {
       showClosed ? true : undefined
     );
 
-    this.academias.emit(
-      academias.filter((value) => {
-        return value.opened === false;
-      })
-    );
+    const filteredAcademias = academias.filter((value) => !value.opened);
+
+    this.updateResultadosCount(filteredAcademias);
+    this.academias.emit(filteredAcademias);
   }
 }
-/// posso criar metodos para caso tenha mascara e afins mas tem que arrumar esse dos horarios
-// posso criar uma pasta com todos os metodos de filtro e no forms passar so um public com os metodos usados para assim diminuir os codigos e ajudar na manutenção do código incluido
