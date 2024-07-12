50aa5495a661fe978713005080cf678ac293c152
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-07-12 10:29:21 -0300

    declarations

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector:'footer-component',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  
+
 }
