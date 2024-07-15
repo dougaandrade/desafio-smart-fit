7900646162c34fa6d6767e693a917df0cb188f9a
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-07-15 10:03:15 -0300

    app-config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
- import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,5 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
-  provideClientHydration()]
+    provideClientHydration(),
+  ],
 };
