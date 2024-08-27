e3401f0d1232212a35cfb2952e154055f4987945
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-08-27 09:26:43 -0300

    bug/master

--- src/app/services/methods-filter.service.ts
+++ src/app/services/methods-filter.service.ts
@@ -1,12 +1,14 @@
 import { FormBuilder} from '@angular/forms';
 import { GetUnitsService } from './get-units.service';
 import { Academias } from '../Interfaces/Ilocation.interface';
-import { EventEmitter, inject, Injectable, Output } from '@angular/core';
+import { EventEmitter, inject, Injectable, Output} from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MethodsFilter {
+
+  // esse decorator so funciona em services
   @Output() filtersmethods = new EventEmitter<Academias[]>();
 
   resultadosCount = 0;
@@ -19,7 +21,6 @@ export class MethodsFilter {
     showClosed: false,
     uf: '',
   });
-
 
   updateResultadosCount(academias: Academias[]) {
     this.resultadosCount = academias.length;
