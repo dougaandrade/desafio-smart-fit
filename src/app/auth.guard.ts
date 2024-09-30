70e50afe96b7f3f29f2ee123c157117937eab8fd
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-09-30 11:58:01 -0300

    guard-auth

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,17 +1,17 @@
 import { inject } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { CanActivateFn, Router } from '@angular/router';
-import Swal from 'sweetalert2';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const AuthServ = inject(AuthService);
   const router = inject(Router);
 
   if (AuthServ.isAuthenticated()) {
+    console.log('Authenticated');
     return true;
   } else {
     router.navigate(['/login']);
-    console.log('nao autenticado');
+    console.log('Not Authenticated');
     return false;
   }
 };
--- src/app/components/pages/not-found-page/not-found-page.component.ts
+++ src/app/components/pages/not-found-page/not-found-page.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'not-found-page',
