382c8d32e8c0604caa12d53e7553cdbe50d2b62b
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-10-29 10:18:01 -0300

    Feature⚙️:404

--- src/app/components/pages/not-found-page/not-found-page.component.ts
+++ src/app/components/pages/not-found-page/not-found-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'not-found-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
 })
