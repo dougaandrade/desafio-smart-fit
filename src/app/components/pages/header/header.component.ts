a8f737971e36d097d8de1ff0040ceed8885d3179
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-10-30 11:24:38 -0300

    master

--- src/app/components/pages/header/header.component.ts
+++ src/app/components/pages/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ModalComponent } from './modal/modal.component';
 import { ButtonIconComponent } from './button-icon/button-icon.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'header-component',
   standalone: true,
-  imports: [ModalComponent, ButtonIconComponent, CommonModule],
+  imports: [ModalComponent, ButtonIconComponent, CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
