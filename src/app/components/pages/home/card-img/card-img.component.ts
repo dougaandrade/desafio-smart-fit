a5646204764ebfd32e99e12c0ed050bd44dd2da7
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-11-22 12:01:18 -0300

    wip🏗️:homePage

---
+++ src/app/components/pages/home/card-img/card-img.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cardImg',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-img.component.html',
+  styleUrl: './card-img.component.scss',
+})
+export class CardImgComponent {}
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ContentMainComponent } from './content-main/content-main.component';
+import { CardImgComponent } from './card-img/card-img.component';
 
 @Component({
   selector: 'homeComponent',
   standalone: true,
-  imports: [ContentMainComponent],
+  imports: [ContentMainComponent, CardImgComponent],
   template: `
     <div class="container-content">
-      <content-main />
+      <!-- <content-main /> -->
+      <cardImg />
     </div>
   `,
   styleUrl: './home.component.scss',
